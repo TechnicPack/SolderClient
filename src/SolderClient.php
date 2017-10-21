0b677bcc9d813a4c36dbcc73af4d7482172cc31d
Pedro Cunha <pedroagracio@gmail.com>
2017-10-21 14:23:40 +0100

    Version 0.1.3

--- src/SolderClient.php
+++ src/SolderClient.php
@@ -21,7 +21,7 @@ class SolderClient
     /** @var Client */
     private $client;
 
-    const VERSION = '0.1.0';
+    const VERSION = '0.1.3';
 
     public static function factory($url, $key, $headers = [], $handler = null, $timeout = 3)
     {
