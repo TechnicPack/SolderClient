a76be0afcb1de98b850de11062f7488e5cc16a4f
Pedro Cunha <pedroagracio@gmail.com>
2024-10-11 16:05:01 +0100

    Release 0.7.3

--- src/SolderClient.php
+++ src/SolderClient.php
@@ -20,7 +20,7 @@ class SolderClient
     public string $key;
     private Client $client;
 
-    const VERSION = '0.7.2';
+    const VERSION = '0.7.3';
 
     public static function factory($url, $key, $headers = [], $handler = null, $timeout = 3): SolderClient
     {
