8d572316c603964383eb570353ec4c0aeeb5bbcd
Pedro Cunha <pedroagracio@gmail.com>
2025-05-21 00:51:34 +0100

    chore: release v0.9.2

--- src/SolderClient.php
+++ src/SolderClient.php
@@ -20,7 +20,7 @@ class SolderClient
     public string $key;
     private Client $client;
 
-    const VERSION = '0.9.1';
+    const string VERSION = '0.9.2';
 
     public static function factory($url, $key, $headers = [], $handler = null, $timeout = 3): SolderClient
     {
